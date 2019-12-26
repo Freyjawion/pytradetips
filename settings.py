5ba3b446d5d45023c163d1d378ce9faca7634e10
Freyjawion <conankyo@hotmail.com>
2019-12-26 15:19:04 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,39 @@
-FETCH_URL = 'https://www.pathofexile.com/api/trade/fetch/{}?query={}'
 SEARCH_API = 'https://www.pathofexile.com/api/trade/search/'
+FETCH_URL = 'https://www.pathofexile.com/api/trade/fetch/{}?query={}'
 LEAGUE = 'Metamorph'
 MAX = 10
+
+NINJA_ITEM_API = 'https://poe.ninja/api/data/itemoverview?league={}&type={}'
+NINJA_CURRENCY_API = 'https://poe.ninja/api/data/currencyoverview?league={}&type={}'
+
+NINJA_CURRENCY = NINJA_CURRENCY_API.format(LEAGUE, 'Currency')
+NINJA_FRAGMENTS = NINJA_CURRENCY_API.format(LEAGUE, 'Fragment')
+NINJA_OILS = NINJA_ITEM_API.format(LEAGUE, 'Oil')
+NINJA_INCUBATORS = NINJA_ITEM_API.format(LEAGUE, 'Incubator')
+NINJA_SCARABS = NINJA_ITEM_API.format(LEAGUE, 'Scarab')
+NINJA_FOSSILS = NINJA_ITEM_API.format(LEAGUE, 'Fossil')
+NINJA_RESONATORS = NINJA_ITEM_API.format(LEAGUE, 'Resonator')
+NINJA_ESSENCES = NINJA_ITEM_API.format(LEAGUE, 'Essence')
+NINJA_DIVINATION_CARDS = NINJA_ITEM_API.format(LEAGUE, 'DivinationCard')
+NINJA_PROPHECIES = NINJA_ITEM_API.format(LEAGUE, 'Prophecy')
+NINJA_SKILL_GEMS = NINJA_ITEM_API.format(LEAGUE, 'SkillGem')
+NINJA_BASE_TYPES = NINJA_ITEM_API.format(LEAGUE, 'BaseType')
+NINJA_HELMET_ENCHANTS = NINJA_ITEM_API.format(LEAGUE, 'HelmetEnchant')
+NINJA_UNIQUE_MAPS = NINJA_ITEM_API.format(LEAGUE, 'UniqueMap')
+NINJA_MAPS = NINJA_ITEM_API.format(LEAGUE, 'Map')
+NINJA_UNIQUE_JEWELS = NINJA_ITEM_API.format(LEAGUE, 'UniqueJewel')
+NINJA_UNIQUE_FLASKS = NINJA_ITEM_API.format(LEAGUE, 'UniqueFlask')
+NINJA_UNIQUE_WEAPONS = NINJA_ITEM_API.format(LEAGUE, 'UniqueWeapon')
+NINJA_UNIQUE_ARMOURS = NINJA_ITEM_API.format(LEAGUE, 'UniqueArmour')
+NINJA_UNIQUE_ACCESSORIES = NINJA_ITEM_API.format(LEAGUE, 'UniqueAccessory')
+NINJA_BEASTS = NINJA_ITEM_API.format(LEAGUE, 'Beast')
+
+dict_rarity = {
+    '传奇': 'Unique',
+    '稀有': 'Rare',
+    '魔法': 'Magic',
+    '普通': 'Normal',
+    '命运卡': 'Card',
+    '通货': 'Currency',
+    '宝石': 'Gem'
+}
